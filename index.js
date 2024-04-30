3c234138e5cad6d59cea028f8e0e36ab59412249
Bushra Asim <160628669+Bushraacoder@users.noreply.github.com>
2024-04-30 14:18:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\Welcome to alish - CLI NUMBER GUESSING GAME\n");
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt(({
+    name: "UserGuessedNumber",
+    type: "input",
+    message: "Enter your guess number(number limit from 1 to 5)!",
+}));
+if (answer.UserGuessedNumber === randomnumber) {
+    console.log("congratulation!you guess a correct number.");
+}
+else {
+    console.log("Sorry, you guess a wrong number.");
+}
